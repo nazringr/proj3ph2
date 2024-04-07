b99eb0e4fb4c3e494bcf20f921bf8d399167b8a1
nazringr <nazrin@umd.edu>
2024-04-07 03:36:07 +0200

    Implemented main part
    Input reception, map creation, running the algorithm

--- part1/aStar_Nazrin_Onkar.py
+++ part1/aStar_Nazrin_Onkar.py
@@ -150,3 +150,200 @@ def a_star_function(pos,ul,ur):
                         return
             UncheckedList.put((TotalCost,Eucledian_dist,Cost,(round(Xn),round(Yn),round(theta_n))))
             Pth[(round(Xn),round(Yn),round(theta_n))] = (old_x,old_y,theta)
+        
+    
+#left wheel rpm = 0 and right wheel rpm = rpm1
+def zero_n_rpm1(a):
+    pos = a[3]
+    a_star_function(pos,0,vel_1)
+
+#left wheel rpm = rpm1 and right wheel rpm = 0
+def rpm1_n_zero(a):
+    pos = a[3] 
+    a_star_function(pos,vel_1,0)    
+    
+#left wheel rpm = rpm1 and right wheel rpm = rpm1
+def rpm1_n_rpm1(a):
+    pos = a[3]
+    a_star_function(pos,vel_1,vel_1) 
+
+# left wheel rpm = zero and right wheel rpm = rpm2
+def zero_n_rpm2(a):
+    pos = a[3]  
+    a_star_function(pos,0,vel_2)
+# left wheel rpm = rpm2 and right wheel rpm = zero
+def rpm2_n_zero(a):
+    pos = a[3]  
+    a_star_function(pos,vel_2,0)
+
+#left wheel rpm = rpm2 and right wheel rpm = rpm2
+def rpm2_n_rpm2(a):
+    pos = a[3] 
+    a_star_function(pos,vel_2,vel_2)
+
+#left wheel rpm = rpm1 and right wheel rpm = rpm2
+def rpm1_n_rpm2(a):
+    pos = a[3] 
+    a_star_function(pos,vel_1,vel_2)
+    
+def rpm2_n_rpm1(a):
+    pos = a[3] 
+    a_star_function(pos,vel_2,vel_1)
+
+#Converting the rpm to velocity
+def rpm_to_velocity(rpm1,rpm2):
+    v1 = (2*np.pi*Wheel_Radius*rpm1)/60
+    v2 = (2*np.pi*Wheel_Radius*rpm2)/60
+    return v1,v2
+
+
+#Defining the bactracking algorithm 
+def B_tracking(Pth,final_val,initial_pt):
+    b_track = []
+    z = list(Pth)[-1]
+    K = Pth.get(z)
+    b_track.append(z)
+    b_track.append(K)
+    print(b_track)
+    while K != (initial_pt):  
+        K = Pth.get(K)
+        b_track.append(K)
+    b_track.reverse()
+    return (b_track)
+
+def PromptPredefined():
+    print("Want to use predefined start, goal and rpms?")
+    ans = input("y/n? ")
+    if ans == "y":
+        return True
+    else:
+        return False
+         
+space = np.ones((2001,6001,3),dtype='uint8')  #Creating an matrix with ones, of the shape of boundry shape
+
+# if PromptPredefined() == False:
+#     Obstacle_Clearance = int(input("Enter the Obstacle Clearance of the Robot: "))
+#     obstacle_space(space)           #Creating the obstacle boundries
+#     Obs_Coords= boundry_creation(space)
+
+#     initial_pt = User_Inputs_Start(Obs_Coords)  
+#     goal_pt = User_Inputs_Goal(Obs_Coords)
+#     rpm_1,rpm_2 = User_Input_rpm()
+#     vel_1,vel_2 = rpm_to_velocity(rpm_1,rpm_2)
+
+#     start = (0,0,0,initial_pt)
+#     InitialEucledian_dist = np.sqrt(((goal_pt[0] - start[3][0])*2)+((goal_pt[1] - start[3][1])*2))  
+#     InitialTotalCost = InitialEucledian_dist   
+#     start = (InitialTotalCost,InitialEucledian_dist,0,initial_pt)
+# else: 
+Obstacle_Clearance = 5
+obstacle_space(space)
+Obs_Coords= boundry_creation(space)
+
+
+
+
+# cv.imshow("SPACE", space )
+# cv.waitKey()
+# cv.destroyAllWindows()
+
+initial_pt = (500,1000, 0)
+# # goal_pt = (225, 150)
+goal_pt = (5750, 1000)
+rpm1 = 150
+rpm2 = 180
+vel_1, vel_2 = rpm_to_velocity(rpm1, rpm2)
+start = (0, 0, 0, initial_pt)
+print(goal_pt)
+InitialEucledian_dist = np.sqrt(((goal_pt[0] - start[3][0])**2)+((goal_pt[1] - start[3][1])**2))
+print(InitialEucledian_dist)
+InitialTotalCost = InitialEucledian_dist
+start = (InitialTotalCost, InitialEucledian_dist, 0, initial_pt)
+# print("start node: ", initial_pt, "\ngoal node: ", goal_pt, "\nrpm1: ", rpm1, "rpm2: ", rpm2)
+print("start")
+print(start)
+
+UncheckedList.put(start)
+#print(UncheckedList)
+reached=0
+while UncheckedList.qsize() != 0:
+    a = UncheckedList.get()
+    print(a)
+    
+    #print(goal_pt)
+    if CheckedList[int(round(a[3][1])), int(round(a[3][0]))] != 1:
+        CheckedList[int(round(a[3][1])) , int(round(a[3][0]))] = 1
+        if a[1] > 5:
+            zero_n_rpm1(a)     
+            rpm1_n_zero(a)
+            rpm1_n_rpm1(a)
+            zero_n_rpm2(a)
+            rpm2_n_zero(a)
+            rpm2_n_rpm2(a)
+            rpm1_n_rpm2(a)
+            rpm2_n_rpm1(a)
+            # for i in CloseList:
+            #     j = Pth.get(i)
+            #     cv.line(space,(i[0],250-i[1]),(j[0],250-j[1]),(0,0,255),1)
+                
+            #     cv.imshow("Space",space)
+            #     if cv.waitKey(50) & 0xFF == ord('q'):
+            #         break
+
+        else:
+            
+            print("success")
+            reached=1
+            break
+
+
+# cv.imshow("SPACE", space )
+# cv.waitKey()
+
+# cv.destroyAllWindows()
+if reached ==1:
+    print("hello")
+    b = B_tracking(Pth,a, initial_pt)
+    
+    
+    print("path")
+    print(b)
+    
+    
+    # for i in CheckedList:
+    #     space[250-i[1]][i[0]] = [255,0,0]
+
+    #     cv.imshow("SPACE", space )
+    # # cv.waitKey(0)
+    
+    #     if cv.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    # print("Closed list")
+    for i in CloseList:
+        xi,yi,teta = i[0],i[1],i[2]
+        j = Pth.get((xi,yi,teta))
+        cv.line(space,(int(i[0]), 2000-int(i[1])),(int(j[0]),2000-int(j[1])),(0,0,255),1)
+        
+        #cv.imshow("Space",space)
+        SPACEscaledUp = cv.resize(space,(1202,402))
+        cv.imshow("Map",SPACEscaledUp)
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    for j in b:
+        space[2000-int(j[1])][int(j[0])] = [0,255,0]
+        plt.scatter(int(j[0]), int(j[1]),color="blue")
+        cv.circle(space,(int(j[0]),2000-int(j[1])), 5, (0,255,0), -1)
+        SPACEscaledUp = cv.resize(space,(1202,402))
+        cv.imshow("SPACE", SPACEscaledUp)
+        if cv.waitKey(50) & 0xFF == ord('q'):
+            break
+
+
+else:
+    print("The Goal node cannot be reached")
+
+
+cv.destroyAllWindows()
+endTime = time.time()
+print("\nrun time = ", endTime - startTime, "seconds")
